47330b571afbb2695d7cc71950bb0d9a18298ba5
Kasper <kasperkh.kh@gmail.com>
2022-04-19 13:45:28 +0200

    Auto dark mode

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
   import { Color } from '$lib/color'
   import ColorInput from '$lib/ColorInput.svelte'
+  import { TinyColor } from '@ctrl/tinycolor'
 
   let color = new Color('#00ff9d')
-  $: console.log(color)
+  $: darkMode = !whiteForegroundWorks(color.toHex())
+
+  export function whiteForegroundWorks(hex: string) {
+    return new TinyColor(hex).getBrightness() < 127.5
+  }
 </script>
 
-<div class="center">
+<div class="center" class:dark-mode={darkMode}>
   <h1 style:color={color.toHex()}>Color Picker Svelte</h1>
 
   <ColorInput bind:color title="Text color" --input-width="250px" />
 </div>
 
 <style lang="sass">
+  :global(html, body)
+    height: 100%
+    margin: 0px
   .center
     display: flex
     flex-direction: column
     align-items: center
     color: #000000
+    height: 100%
+    padding: 100px 0px
+    font-family: Arial, Helvetica, sans-serif
+    font-size: 16px
+    background-color: #ffffff
+    transition: background-color, 200ms ease-out
+  .dark-mode
+    background-color: #111318
 </style>
