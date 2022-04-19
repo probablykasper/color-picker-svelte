434ae3465b4301a3680a87249a0fda24934485c6
Kasper <kasperkh.kh@gmail.com>
2022-04-19 04:51:44 +0200

    SvelteKit setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Color Picker Svelte
+
+## Dev instructions
+
+### Commands
+
+- `npm run dev`: Start in dev mode
+- `npm run build`: Build
+- `npm run preview`: Preview production app
+- `npm run lint`: Lint
+- `npm run format`: Format
---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<h1>Color Picker Svelte</h1>
+
+<style lang="sass">
+  :global(body)
+    margin: 100px 0px
+    font-family: Arial, Helvetica, sans-serif
+    font-size: 18px
+    background-color: #111318
+    color: #f2f2f2
+    text-align: center
+  h1
+    color: #00ff9d
+</style>
